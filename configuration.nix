974c7bb0ce028bf8df680607fe7ece30b801cacc
Daniele Ieva <52803074+Asdrei@users.noreply.github.com>
2023-08-28 11:38:49 +0200

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,95 @@
+{ pkgs, config, ... }:
+
+{
+	# Import hardware configuration
+	imports = [
+		./hardware-configuration.nix
+	];
+	# Configure boot
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+	boot.loader.timeout = 0;
+
+	# Configure basic options
+	networking.hostName = "nixos";
+	time.timeZone = "Europe/Rome";
+
+	i18n.defaultLocale = "en_US.UTF-8";
+	console = {
+		font = "Lat2-Terminus16";
+		keyMap = "it";
+	};
+
+	# Install system wide programs
+	environment.systemPackages = with pkgs; [
+		wget
+		micro
+		git
+		gh
+		distrobox
+		man
+		lsd
+	];
+
+	# enable and configure zsh
+	programs.zsh = {
+		enable = true;
+		autosuggestions.enable = true;
+		ohMyZsh = {
+			enable = true;
+			theme = "crunch";
+		};
+		shellAliases = {
+			ls = "lsd";
+			ll = "lsd -l";
+			la = "lsa -lA";
+			rm = "rm -r";
+		};
+	};
+	environment.shells = [
+		pkgs.zsh
+		pkgs.bash
+	];
+
+	# Enable and configure podman for distrobox
+	virtualisation.podman = {
+		enable = true;
+		autoPrune.enable = true;
+		dockerCompat = true;
+	};
+
+	# Configure btrfs trim
+	services.btrfs.autoScrub = {
+		enable = true;
+		interval = "weekly";
+	};
+
+	# Print ip address on tty login to ssh
+	environment.etc."issue.d/ip.issue".text = "\\4\n";
+	networking.dhcpcd.runHook = "${pkgs.utillinux}/bin/agetty --reload";
+
+	# Enable openssh daemon
+	services.openssh.enable = true;
+	networking.firewall.allowedTCPPorts = [ 22 ];
+
+	# Enable doas (a sudo alternative)
+	security.doas = {
+		enable = true;
+		wheelNeedsPassword = true;
+	};
+	# User Configuration
+	users.users = {
+		admin = {
+			isNormalUser = true;
+			extraGroups = [ "wheel" ];
+			group = "users";
+			home = "/home/admin";
+		};
+	};
+
+	users.defaultUserShell = pkgs.zsh;
+
+	# Final System Configuration
+	system.copySystemConfiguration = true;
+	system.stateVersion = "23.05";
+}
